88a8c77d6199ba0b246bd397cbf9964b5a787db9
minzzun99 <skg06123@naver.com>
2025-11-10 16:43:11 +0900

    feat: 아웃 카운트, 스트라이크 카운트 관리하는 Count 클래스 구현
    - 스트라이크, 아웃 카운트 관리
    - 3스트라이크 -> 아웃
    - 3아웃 초과 시 예외 발생

---
+++ src/main/java/com/woowaprecourse/minjun_baseball_game/domain/Count.java
@@ -0,0 +1,50 @@
+package com.woowaprecourse.minjun_baseball_game.domain;
+
+public class Count {
+    private static final int MAX_STRIKE = 3;
+    private static final int MAX_OUT = 3;
+
+    private int strike;
+    private int out;
+
+    public Count() {
+        this.strike = 0;
+        this.out = 0;
+    }
+
+    public void addStrike() {
+        strike++;
+
+        if (isStrikeOut()) {
+            addOut();
+            resetStrike();
+        }
+    }
+
+    public void addOut() {
+        if (isThreeOut()) {
+            throw new IllegalArgumentException("이미 3아웃입니다.");
+        }
+        out++;
+    }
+
+    private void resetStrike() {
+        this.strike = 0;
+    }
+
+    public boolean isStrikeOut() {
+        return strike >= MAX_STRIKE;
+    }
+
+    public boolean isThreeOut() {
+        return out >= MAX_OUT;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getOut() {
+        return out;
+    }
+}
