96bcb238861070cfa477a0bd174984dbebaccca6
minzzun99 <skg06123@naver.com>
2025-11-13 15:37:59 +0900

    refactor: BaseballGameService 클래스 명, playPitch 메서드 수정
    - 투수 모드일 경우 선택한 곳으로 투구하는 로직 추가
    - 타자 모드일 경우 랜덤으로 투구하는 로직 추가
    - BaseballGameService -> BaseballGame 네이밍 수정

--- src/main/java/com/woowaprecourse/minjun_baseball_game/domain/BaseballGame.java
+++ src/main/java/com/woowaprecourse/minjun_baseball_game/domain/BaseballGame.java
@@ -3,24 +3,26 @@ package com.woowaprecourse.minjun_baseball_game.domain;
 import com.woowaprecourse.minjun_baseball_game.domain.strategy.NumberGenerator;
 import com.woowaprecourse.minjun_baseball_game.domain.strategy.ZoneStrategy;
 
-public class BaseballGameService {
+public class BaseballGame {
     private final GameMode gameMode;
     private final GameState gameState;
     private final PitchResultJudge judge;
     private final ZoneStrategy zoneStrategy;
+    private final ZoneRandomGenerator zoneRandomGenerator;
 
-    public BaseballGameService(GameMode gameMode, BattingRecord battingRecord, ZoneStrategy zoneStrategy,
-                               NumberGenerator numberGenerator) {
+    public BaseballGame(GameMode gameMode, BattingRecord battingRecord, ZoneStrategy zoneStrategy,
+                        NumberGenerator numberGenerator, ZoneRandomGenerator zoneRandomGenerator) {
         this.gameMode = gameMode;
         this.gameState = new GameState(gameMode, numberGenerator);
         this.judge = new PitchResultJudge(battingRecord, numberGenerator);
         this.zoneStrategy = zoneStrategy;
+        this.zoneRandomGenerator = zoneRandomGenerator;
     }
 
     public void playPitch(int selectedZone) {
         validateGameNotOver();
 
-        Zone userZone = new Zone(selectedZone);
+        Zone userZone = getPitchedZone(selectedZone);
         StrikeZone strikeZone = zoneStrategy.generate(selectedZone);
 
         PitchResult result = judge.judge(userZone, strikeZone);
@@ -71,5 +73,14 @@ public class BaseballGameService {
         if (!isGameOver()) {
             throw new IllegalStateException("게임이 진행 중입니다.");
         }
+    }
+
+    private Zone getPitchedZone(int selectedZone) {
+        // 투수 모드 - 선택한 곳으로 투구
+        if (gameMode == GameMode.PITCHER) {
+            return new Zone(selectedZone);
+        }
+        // 타자 모드 - 랜덤으로 투구 (selectedZone => 핫존)
+        return zoneRandomGenerator.randomZone();
     }
 }
