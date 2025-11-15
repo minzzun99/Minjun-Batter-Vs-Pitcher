3ae40e2d8f90142f1432d8d5b291eff271de8b28
minzzun99 <skg06123@naver.com>
2025-11-15 16:31:44 +0900

    feat: Player 관련 DTO 추가
    - PlayerDto -> 선수 정보 응답
    - PlayerCreateRequest -> 선수 생성 요청
    - GameStartRequest playerId 필드 추가
    - PlayerUpdateRequest -> 선수 기록 수정 요청

---
+++ backend/src/main/java/com/woowaprecourse/minjun_baseball_game/dto/PlayerCreateRequest.java
@@ -0,0 +1,28 @@
+package com.woowaprecourse.minjun_baseball_game.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PlayerCreateRequest {
+    @NotBlank
+    private String name;
+
+    @Min(1)
+    private int totalAtBats;
+
+    @Min(0)
+    private int singles;
+
+    @Min(0)
+    private int doubles;
+
+    @Min(0)
+    private int triples;
+
+    @Min(0)
+    private int homeRuns;
+}
---
+++ backend/src/main/java/com/woowaprecourse/minjun_baseball_game/dto/PlayerUpdateRequest.java
@@ -0,0 +1,30 @@
+package com.woowaprecourse.minjun_baseball_game.dto;
+
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayerUpdateRequest {
+    @NotBlank
+    private String name;
+
+    @Min(1)
+    private int totalAtBats;
+
+    @Min(0)
+    private int singles;
+
+    @Min(0)
+    private int doubles;
+
+    @Min(0)
+    private int triples;
+
+    @Min(0)
+    private int homeruns;
+}
