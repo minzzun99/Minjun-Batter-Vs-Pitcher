511f03cac96b42271982e5b3edc84eb17c9244a6
minzzun99 <skg06123@naver.com>
2025-11-15 16:35:51 +0900

    refactor: BattingRecord 생성자 수정
    - 하드 코딩된 기록 삭제
    - 생성자 파라미터 추가

--- backend/src/main/java/com/woowaprecourse/minjun_baseball_game/domain/BattingRecord.java
+++ backend/src/main/java/com/woowaprecourse/minjun_baseball_game/domain/BattingRecord.java
@@ -4,18 +4,26 @@ import com.woowaprecourse.minjun_baseball_game.domain.strategy.NumberGenerator;
 import lombok.Getter;
 
 public class BattingRecord {
-    private static final int TOTAL_AT_BATS = 166;
-    private static final int SINGLES = 33;
-    private static final int DOUBLES = 23;
-    private static final int TRIPLES = 4;
-    private static final int HOMERUNS = 1;
-
     @Getter
     private final String playerName;
+
+    private final int totalAtBats;
+    private final int singles;
+    private final int doubles;
+    private final int triples;
+    private final int homeruns;
+
     private final NumberGenerator numberGenerator;
 
-    public BattingRecord(String playerName, NumberGenerator numberGenerator) {
+
+    public BattingRecord(String playerName, int totalAtBats, int singles, int doubles, int triples, int homeruns,
+                         NumberGenerator numberGenerator) {
         this.playerName = playerName;
+        this.totalAtBats = totalAtBats;
+        this.singles = singles;
+        this.doubles = doubles;
+        this.triples = triples;
+        this.homeruns = homeruns;
         this.numberGenerator = numberGenerator;
     }
 
@@ -35,22 +43,22 @@ public class BattingRecord {
     }
 
     public double getBattingAverage() {
-        return (double) getTotalHits() / TOTAL_AT_BATS;
+        return (double) getTotalHits() / totalAtBats;
     }
 
     public int getTotalHits() {
-        return SINGLES + DOUBLES + TRIPLES + HOMERUNS;
+        return singles + doubles + triples + homeruns;
     }
 
     private double getSingleRate() {
-        return (double) SINGLES / getTotalHits();
+        return (double) singles / getTotalHits();
     }
 
     private double getDoubleRate() {
-        return (double) (SINGLES + DOUBLES) / getTotalHits();
+        return (double) (singles + doubles) / getTotalHits();
     }
 
     private double getTripleRate() {
-        return (double) (SINGLES + DOUBLES + TRIPLES) / getTotalHits();
+        return (double) (singles + doubles + triples) / getTotalHits();
     }
 }
