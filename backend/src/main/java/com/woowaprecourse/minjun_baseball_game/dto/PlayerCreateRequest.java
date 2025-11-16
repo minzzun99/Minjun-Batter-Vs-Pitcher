337ab48684007390adf70aaa2e934bc6cc3abeab
minzzun99 <skg06123@naver.com>
2025-11-16 16:39:33 +0900

    refactor: 선수 상세 타격 기록 필드 추가

--- backend/src/main/java/com/woowaprecourse/minjun_baseball_game/dto/PlayerCreateRequest.java
+++ backend/src/main/java/com/woowaprecourse/minjun_baseball_game/dto/PlayerCreateRequest.java
@@ -2,11 +2,13 @@ package com.woowaprecourse.minjun_baseball_game.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PlayerCreateRequest {
     @NotBlank
     private String name;
